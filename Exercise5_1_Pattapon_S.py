05cfa34802cffe465740df06cb7d84fca57f03b7
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-16 15:58:09 +0700

    Update Exercise5_1_Pattapon_S.py

--- Exercise5_1_Pattapon_S.py
+++ Exercise5_1_Pattapon_S.py
@@ -1,1 +1,6 @@
-
+num_1 = int(input("First Number : "))
+num_2 = int(input("Second Number : "))
+print(num_1, "+", num_2, "=", num_1 + num_2)
+print(num_1, "-", num_2, "=", num_1 - num_2)
+print(num_1, "*", num_2, "=", num_1 * num_2)
+print(num_1, "/", num_2, "=", num_1 / num_2)
