acbffb0c2f37cd24ae3856c6356f0e480ae5c982
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-16 10:03:21 +0700

    Create Exercise4_Pattpon_P.py

---
+++ Exercise4_Pattpon_P.py
@@ -0,0 +1,9 @@
+Found_Eng = 80
+Gene_Bus = 89
+Intro_CS = 90
+CS_pro = 91 
+print("**********Ur Score**********")
+print("Foundation English: ",Found_Eng)
+print("General Business: ",Gene_Bus)
+print("Introduction to Computer Systems: ",Intro_CS)
+print("Computer Programming: ",CS_pro)
