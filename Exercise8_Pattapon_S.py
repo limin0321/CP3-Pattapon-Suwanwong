50e49efe634514fe96658465721acd684a9fcb8a
limin0321 <147285602+limin0321@users.noreply.github.com>
2023-11-17 13:15:08 +0700

    Update Exercise8_Pattapon_S.py

--- Exercise8_Pattapon_S.py
+++ Exercise8_Pattapon_S.py
@@ -19,17 +19,17 @@ if userName == "Naomi" and passWord == "123456":
     if fruit == "Apple":
         num_1 = 20
         calcu = input("How many pieces? : ")
-        result = num_1 * 1.07 *int(calcu)
+        result = num_1 * 1.07 *float(calcu)
         print("Summary(THB): ",result)
     elif fruit == "Mango":
         num_2 = 65
         calcu = input("How many pieces? : ")
-        result_2 = num_2 * 1.07 *int(calcu)
+        result_2 = num_2 * 1.07 *float(calcu)
         print("Summary(THB): ",result_2)
     elif fruit == "Orange":
         num_3 = 45
         calcu = input("How many pieces? : ")
-        result_3 = num_3 * 1.07 *int(calcu)
+        result_3 = num_3 * 1.07 *float(calcu)
         print("Summary(THB): ",result_3)
     else:
         print("Bye bye")
