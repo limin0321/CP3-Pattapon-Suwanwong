20d058ea8317f888a2c8d7eb15043484e63333fa
limin0321 <147285602+limin0321@users.noreply.github.com>
2024-01-07 21:42:33 +0700

    2024/01/07

---
+++ Lecture_50.py
@@ -0,0 +1,15 @@
+def addnumber(x,y):
+    print(x+y)
+addnumber(1,2)
+
+def Subnumber(x,y):
+    print(x-y)
+Subnumber(10,1)
+
+def Mulnumber(x,y):
+    print(x*y)
+Mulnumber(2,3)
+
+def Divnumber(x,y):
+    print(x/y)
+Divnumber(10,5)
